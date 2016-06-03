c766233d10b49254e64d17ecf0cb0d5c8076e833
Pavel Aleksandrov <inblank@yandex.ru>
2016-06-03 15:35:46 +0300

    Fix error in initial migration

--- migrations/m160523_164512_activeuser_init.php
+++ migrations/m160523_164512_activeuser_init.php
@@ -17,20 +17,20 @@ class m160523_164512_activeuser_init extends Migration
             'status' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
 
             'email' => Schema::TYPE_STRING . "(200) NOT NULL DEFAULT ''",
-            'pass_hash' => Schema::TYPE_STRING . "(40) NOT NULL DEFAULT ''",
+            'pass_hash' => Schema::TYPE_STRING . "(60) NOT NULL DEFAULT ''",
 
-            'avatar' => Schema::TYPE_STRING . "(45) NOT NULL DEFAULT ''",
             'name' => Schema::TYPE_STRING . "(200) NOT NULL DEFAULT ''",
             'gender' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
             'birth' => Schema::TYPE_DATE . " DEFAULT NULL",
+            'avatar' => Schema::TYPE_STRING . "(45) NOT NULL DEFAULT ''",
 
             'access_token' => Schema::TYPE_STRING . "(40) DEFAULT NULL",
             'auth_key' => Schema::TYPE_STRING . "(40) DEFAULT NULL",
 
-            'registered_at' => Schema::TYPE_DATETIME . ' DEFAULT NULL',
-
             'token' => Schema::TYPE_STRING . "(40) DEFAULT NULL",
             'token_created_at' => Schema::TYPE_INTEGER . " NOT NULL DEFAULT 0",
+
+            'registered_at' => Schema::TYPE_DATETIME . ' DEFAULT NULL',
         ], $this->tableOptions);
         $this->createIndex('unique_email', $tableUsers, 'email', true);
         $this->createIndex('unique_access_token', $tableUsers, 'access_token', true);
